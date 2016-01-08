e35cebdedfef5b159482a77fb14b16b294764c05
NormanBenbrahim <norman.benbrahim@gmail.com>
2016-01-08 16:08:58 -0500

    working solution to assignment2

--- course_content/rprogramming/assign2/ProgrammingAssignment2/cachematrix.R
+++ course_content/rprogramming/assign2/ProgrammingAssignment2/cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Creates a special cached object if the inverse of a matrix exists,
+# otherwise it solves it using 'solve' and caches it for later use
 
-## Write a short comment describing this function
-
+## Assuming our matrix is always square we can find the inverse of A using
+## the built in solve(A) function in R
 makeCacheMatrix <- function(x = matrix()) {
+	m <- NULL # initialize inverse
 
+	# set matrix value
+	set <- function(y) {
+		x <<- y 
+		m <<- NULL
+	}
+
+	# get matrix value
+	get <- function() x
+
+	# set and get the inverse value respectively
+	set_inverse <- function(inverse) m <<- inverse
+	get_inverse <- function() m
+
+	# list of all the functions above 
+	list(set=set, 
+		 get=get, 
+		 set_inverse=set_inverse, 
+		 get_inverse=get_inverse)
 }
 
-
-## Write a short comment describing this function
-
+## I put the other condition in an 'else' clause because an 'if' without an
+## 'else' seems unnatural to me
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$get_inverse() # cached inverse
+        
+        if (!is.null(m)) {
+        	message("Getting cached inverse data ")
+        	return(m)
+        }
+        else { 
+        	data <- x$get() # fetch data object from cache
+        	m <- solve(x, ...) # solve it
+        	x$set_inverse(m) # set new cache object inverse
+        	return(m) # return it 
+        	# ta-da
+        }
 }

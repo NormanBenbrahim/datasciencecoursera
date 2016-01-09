0034b12f68696b88c1a9d052d6096da25f237f44
NormanBenbrahim <norman.benbrahim@gmail.com>
2016-01-08 22:22:59 -0500

    created plot_all script

---
+++ course_content/exploratorydataanalysis/week1/assign1/plot_all.R
@@ -0,0 +1,7 @@
+# to plot all the png files 
+# ::: NOTE the 'household_power_consumption.txt' must be in this :::
+# ::: directory for this script to work							 ::: 
+source("plot1.R")
+source("plot2.R")
+source("plot3.R")
+source("plot4.R")

6aace35dbdd9ca95dda92b0da7cbadc19ff8de28
NormanBenbrahim <norman.benbrahim@gmail.com>
2016-01-23 17:04:38 -0500

    functional code

---
+++ exploratorydataanalysis/week4/assign2/plot4.R
@@ -0,0 +1,35 @@
+# libraries 
+library(ggplot2)
+library(dplyr)
+
+if (!exists("pm25") | !exists("classif")) {
+    source("load_data.R")
+}
+
+# extract the names
+coal_levels <- levels(classif$EI.Sector)
+coal_names <- coal_levels[grep("Coal", coal_levels, ignore.case = T)]
+
+# subset out only coal values
+coal_subset <- subset(classif, EI.Sector %in% coal_names)
+
+# get scc indices
+scc <- coal_subset$SCC
+
+# apply it to the pm25 data
+pm25_subset <- filter(pm25, pm25$SCC %in% as.character(scc))
+
+# sum the year values
+sums <- summarise(group_by(pm25_subset, year), sum(Emissions, na.rm = T))
+
+# rename columns
+names(sums) <- c("Year", "Emissions")
+
+# plot the result
+png("./plot4.png")
+qplot(Year, Emissions, data = sums, geom = "line") + 
+    ggtitle("Total Coal Related Emissions in the US")
+dev.off()
+
+
+

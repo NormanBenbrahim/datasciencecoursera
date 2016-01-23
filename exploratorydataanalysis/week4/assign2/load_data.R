db4521050ed4f927823973b6c5042dac772c1902
NormanBenbrahim <norman.benbrahim@gmail.com>
2016-01-23 17:26:19 -0500

    some typos, added comments

--- exploratorydataanalysis/week4/assign2/load_data.R
+++ exploratorydataanalysis/week4/assign2/load_data.R
@@ -4,6 +4,7 @@ p_file <- "summarySCC_PM25.rds"
 
 # check files exist
 if (!file.exists(c_file) | !file.exists(p_file)) {
+print("Data files not present, downloading and unzipping...")
 dataUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
 tmp <- tempfile()
 download.file(dataUrl, destfile = tmp)
@@ -12,11 +13,11 @@ unlink(tmp)
 }
 
 # load
+print("Loading files...")
 classif <- readRDS(c_file)
 pm25 <- readRDS(p_file)
 
 # cleanup
 pm25$year <- as.Date(as.character(pm25$year), format = "%Y")
 pm25$type <- as.factor(pm25$type)
-
-
+print("Done")

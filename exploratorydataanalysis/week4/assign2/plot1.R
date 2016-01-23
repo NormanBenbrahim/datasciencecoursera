3ee770dcf3258152a6b3c55c98f0fa4f67d48949
NormanBenbrahim <norman.benbrahim@gmail.com>
2016-01-23 15:10:53 -0500

    working version

--- exploratorydataanalysis/week4/assign2/plot1.R
+++ exploratorydataanalysis/week4/assign2/plot1.R
@@ -10,10 +10,11 @@ sums <- summarise(group_by(pm25, year), sum(Emissions))
 
 # plot and save it 
 png("./plot1.png")
-barplot(sums$`sum(Emissions)`, 
-        names = sums$year,
-        col = c("darkgreen", "blue", "red", "purple"),
-        ylab = "Total Emissions",
-        xlab = "Year",
-        main = "Total Emissions for the US")
+plot(sums, 
+     col = "purple",
+     lwd = 2,
+     type = "l",
+     ylab = "Total Emissions",
+     xlab = "Year",
+     main = "Total Emissions for the US")
 dev.off()
